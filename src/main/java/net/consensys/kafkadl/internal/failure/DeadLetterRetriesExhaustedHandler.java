26f22f77b0205cf8d1d2ba2f6daba543c3027c4c
Craig <craigwilliams84@googlemail.com>
2019-01-30 14:52:36 +0000

    Added ability to add custom DeadLetterRetriesExhausted handlers to the application context in order to act on this event.

--- src/main/java/net/consensys/kafkadl/internal/failure/DeadLetterRetriesExhaustedHandler.java
+++
@@ -1,7 +0,0 @@
-package net.consensys.kafkadl.internal.failure;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface DeadLetterRetriesExhaustedHandler {
-    void onFailure(ConsumerRecord record);
-}
